b7891a45554ddb4f80b515ec520970af05cb6995
Silvan Moura <silvancortesmoura@outlook.com>
2023-04-18 23:32:27 -0300

    Implementing Vue Router - part 1

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <Router-link to="/">
+            <img src="" id="logo">
+        </Router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </nav>
+</template>
+
+<script>
+    export default{
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
