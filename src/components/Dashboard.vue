457f8fa28a211fb8b27873c03382a4db0b381973
Silvan Moura <silvancortesmoura@outlook.com>
2023-04-28 21:43:16 -0300

    Removing orders from the system

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -33,7 +33,7 @@
                             {{ burgerStatus.tipo }}
                         </option>
                     </select>
-                    <button class="delete-btn">Cancelar</button>
+                    <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
             </div>
 
@@ -68,6 +68,15 @@
                 const data = await req.json();
 
                 this.status = data;
+            },
+            async deleteBurger(id){
+                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                    method: "DELETE"
+                });
+
+                const res = await req.json();
+
+                this.getPedidos();
             }
         },
         mounted() {
