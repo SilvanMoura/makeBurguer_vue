a82a3eedc06a1da8deae1b67102ed8bb84df707a
Silvan Moura <silvancortesmoura@outlook.com>
2023-05-05 15:01:20 -0300

    Refactoring navbar with login and registration

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,33 +1,60 @@
 <template>
-    <nav>
+    <header>
+
         <Router-link id="logo-url" to="/">
             <img :src="logo" :alt="alt" id="logo">
         </Router-link>
-        <router-link to="/">Home</router-link>
-        <router-link to="/requests">Pedidos</router-link>
-    </nav>
+
+        <nav>
+            <router-link to="/">Home</router-link>
+            <router-link v-if="!link" to="/requests">Pedidos</router-link>
+        </nav>
+
+        <router-link v-if="link" to="/login" class="btn-auth">Entrar</router-link>
+        <router-link v-if="!link" to="/" @click="logout">Sair</router-link>
+
+    </header>
+    
 </template>
 
 <script>
+    import Cookies from 'js-cookie';
+
     export default{
         name: "Navbar",
-        props: ["logo", "alt"]
+        props: ["logo", "alt"],
+        data(){
+            return{
+                link: true
+            }
+        },
+
+        methods: {
+            loadPage(){
+                const token = Cookies.get("_myapp_token");
+
+                !token ? this.link=true : this.link=false ;
+            },
+            logout(){
+                Cookies.remove("_myapp_token");
+                this.$router.push('/');
+            }
+        },
+        mounted() {
+            this.loadPage()
+        }
     }
 </script>
 
 <style scoped>
-    nav{
+
+    header{
         background-color: #222;
         border-bottom: 4px solid #111;
         padding: 15px 50px;
         display: flex;
-        justify-content: flex-end;
+        justify-content: space-between;
         align-items: center;
-    }
-
-    nav #logo-url{
-        margin: auto;
-        margin-left: 0;
     }
 
     #logo{
@@ -35,14 +62,14 @@
         height: 40px;
     }
 
-    nav a{
+    nav a, .btn-auth{
         color: #FCBA03;
         text-decoration: none;
         margin: 12px;
         transition: .5s;
     }
 
-    nav a:hover{
+    nav a:hover, .btn-auth:hover{
         color: #FFF;
     }
 </style>
