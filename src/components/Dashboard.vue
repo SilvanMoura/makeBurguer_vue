b6c9b2a600d016fdefbbee5bfa4005c1f52bd064
Silvan Moura <silvancortesmoura@outlook.com>
2023-06-29 23:17:02 -0300

    Refactoring request to delete order in DB

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -95,10 +95,10 @@
             },
 
             async deleteBurger(id){
-                const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                const req = await fetch(`http://127.0.0.1:8000/api/burguerDelete/${id}`, {
                     method: "DELETE"
                 });
-
+                
                 const res = await req.json();
 
                 this.msg = `Pedido removido com sucesso`;
